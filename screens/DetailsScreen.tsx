f259d13811c95ec154bdb71739e5f27962d61370
mrKornelius <magnus.korneliusson@gmail.com>
2024-09-30 14:37:09 +0200

    🕵🏻

--- screens/DetailsScreen.tsx
+++ screens/DetailsScreen.tsx
@@ -1,8 +1,6 @@
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
-import { RootStackParamList } from '../App';
 
-type DetailsProps = NativeStackScreenProps<RootStackParamList, 'Details'>;
+// type DetailsProps = NativeStackScreenProps<RootStackParamList, 'Details'>;
 
 export default function DetailsScreen() {
   return (
