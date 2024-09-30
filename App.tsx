183e23bcf7d3e050009e9da31364c5f7d06a8d55
mrKornelius <magnus.korneliusson@gmail.com>
2024-09-30 13:02:22 +0200

    Added navigation and a new screen 🧭

--- App.tsx
+++ App.tsx
@@ -1,12 +1,26 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+import DetailsScreen from "./screens/DetailsScreen";
+import HomeScreen from "./screens/HomeScreen";
+
+export type RootStackParamList = {
+  Home: undefined;
+  Details: undefined;
+};
+
+const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Home</Text>
+    <NavigationContainer>
+      <RootStack.Navigator initialRouteName="Home">
+        <RootStack.Screen name="Home" component={HomeScreen} />
+        <RootStack.Screen name="Details" component={DetailsScreen} />
+      </RootStack.Navigator>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,22 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { RootStackParamList } from "../App";
+
+type HomeProps = NativeStackScreenProps<RootStackParamList, "Home">;
+
+export default function HomeScreen({ navigation }: HomeProps) {
+  return (
+    <View style={s.container}>
+      <Text>Home</Text>
+      <Button title="Details" onPress={() => navigation.navigate("Details")} />
+    </View>
+  );
+}
+
+const s = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
